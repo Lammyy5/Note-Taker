233c1163ca5b3847b702f278aa8cc97ee397dd4b
Lammyy5 <reliford2010@gmail.com>
2023-11-12 13:49:57 -0800

    added a server.js file to connect the routes and folders together also created a routes folder with an index.js file

---
+++ Develop/server.js
@@ -0,0 +1,24 @@
+// required files
+const express = require('express')
+const path = require('path')
+const { clog } = require('./middleware/clog')
+const api = require('./routes/index.js')
+// Port 
+const PORT = process.env.PORT || 3001
+// express app function
+const app = express()
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// Connecting the public folder
+app.use(express.static('public'))
+// Get Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+)
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+)
+
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`))
