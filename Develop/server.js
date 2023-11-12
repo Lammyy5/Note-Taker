4945cf1fc7cbba137a3c7e16ec8ac107d2cab57f
Lammyy5 <reliford2010@gmail.com>
2023-11-12 14:00:40 -0800

    Added a helper file for reading and writing files

--- Develop/server.js
+++ Develop/server.js
@@ -1,18 +1,29 @@
 // required files
+
 const express = require('express')
 const path = require('path')
-const { clog } = require('./middleware/clog')
 const api = require('./routes/index.js')
-// Port 
+
+// Port
+
 const PORT = process.env.PORT || 3001
+
 // express app function
+
 const app = express()
+
 // Middleware for parsing JSON and urlencoded form data
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/api', api)
+
 // Connecting the public folder
+
 app.use(express.static('public'))
+
 // Get Route for homepage
+
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 )
